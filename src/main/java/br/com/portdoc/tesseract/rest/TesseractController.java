b809c9864f2333aca979444447d217967d495c34
Marlon-Santos <marlonergondossantos@gmail.com>
2020-08-01 01:34:54 -0300

    deploy

--- src/main/java/br/com/portdoc/tesseract/rest/TesseractController.java
+++ src/main/java/br/com/portdoc/tesseract/rest/TesseractController.java
@@ -66,7 +66,7 @@ public class TesseractController {
                 fopimage = ipimage;
             }
             Tesseract instance = new Tesseract();
-            instance.setDatapath("src/main/resources/tesseractData");
+            instance.setDatapath("src/main/resources");
             instance.setLanguage("por");
             strRetorno = instance.doOCR(fopimage);
         } catch (Exception e) {
